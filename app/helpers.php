3db433abab8e5573003450577bf3e49ee3bc10b5
Camilo Carvajal <camilocc597@gmail.com>
2022-12-29 04:29:09 +0000

    adding helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,18 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+if (!function_exists("view")) {
 
+    function view($view) {
+        return new App\Http\Response($view);
+    }
+
+}
+
+
+if (!function_exists("viewPath")) {
+
+    function viewPath($view) {
+        return __DIR__ . "/../views/$view.php";
+    }
+
+}
