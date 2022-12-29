01a43531fdd312e58fc6923a3b251e50bfeb8bca
Camilo Carvajal <camilocc597@gmail.com>
2022-12-29 21:22:09 +0000

    adding questions view

---
+++ app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Description of PreguntasController
+ *
+ * @author camil
+ */
+class QuestionsController {
+
+    public function index() {
+        return view('questions');
+    }
+}
---
+++ views/questions.php
@@ -0,0 +1,6 @@
+<div class="col-8">
+    <h1>Questions</h1>
+    <p>
+        Phasellus ex tellus, efficitur vitae dui vel, malesuada consectetur quam. Donec nec vehicula sem. Aliquam ultricies magna ipsum, eget ullamcorper tortor iaculis vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In eget libero tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas facilisis turpis et augue fringilla, congue lobortis est tincidunt. Vivamus tempor at quam vitae tristique. Maecenas eget vestibulum dui, at malesuada dolor. Aliquam eu ante blandit, fermentum ex et, dapibus eros. Nulla laoreet sem ultricies purus ullamcorper congue. 
+    </p>
+</div>
