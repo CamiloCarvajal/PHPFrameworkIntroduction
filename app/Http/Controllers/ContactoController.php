84668f4dbd5d1f3c01ea56971188614b20743d5d
Camilo Carvajal <camilocc597@gmail.com>
2022-12-29 20:21:17 +0000

    creating contact resource

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Description of ContactoController
+ *
+ * @author camil
+ */
+class ContactoController {
+
+    public function index(){
+        return view('contacto');
+    }
+}
